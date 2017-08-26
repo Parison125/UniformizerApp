ae79a0d8ad0d236f332a6dd96d75a4f07d0d7935
Parison Rvlmd <parisonravalomanda@gmail.com>
2017-08-26 13:39:15 +0200

    Begin coding after getting prerequired items

--- src/main/java/org/parison/cool/MainApp.java
+++ src/main/java/org/parison/cool/MainApp.java
@@ -1,19 +1,32 @@
 package org.parison.cool;
 
 import org.apache.camel.main.Main;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.*;
 
 /**
- * A Camel Application
+ * @Parison Rvmld
  */
 public class MainApp {
 
-    /**
-     * A main() so we can easily run these routing rules in our IDE
-     */
+
     public static void main(String... args) throws Exception {
-        Main main = new Main();
-        main.addRouteBuilder(new MyRouteBuilder());
-        main.run(args);
+
+/*        InputStream excelFile = ClassLoader.getSystemResource("tartanpion.txt").openStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(excelFile));
+        String line ="";
+        while ((line = reader.readLine()) != null ) {
+            System.out.println(line);
+        }*/
+
+        String file = "etc/tartanpion.txt";
+        System.out.print("Fichier a lire "+file);
+        File iz = new File(file);
+        FileOutputStream fileout = new FileOutputStream(iz);
+        fileout.write("Contenu qojfokqopcekc".getBytes());
+        fileout.flush();
+        fileout.close();
     }
 
 }
